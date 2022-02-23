2adefe577718d683efc5a51d9c1c47d26275d446
Alexander Baranov <a9116876689@gmail.com>
2022-02-23 20:51:42 +0300

    fin

--- ConsoleColors.cpp
+++ ConsoleColors.cpp
@@ -1,29 +1,32 @@
 ﻿#include <iostream>
-#include <unistd.h>
+#include <windows.h>
 #include <stdlib.h>
+#include <string> 
 
 using namespace std;
 
-int main()
-{
-	using namespace std;
+string a, b, c;
+float x, y, z;
+int i, j, k;
 
-	string a, b, c;
-	float x, y, z;
-	int i, j, k;
 
-	int main() {
-		i = 1;
-		while (true) {
-			sleep(1);
-			system("clear");
-			//a = to_string(rand()%9);
-			//b = to_string(rand()%9);
-			//c = a + b;
-			system("Color 14");
-
-			cout << i << endl << to_string(rand()) << endl;
-			i++;
+int main() {
+	i = 1;
+	while (i < 14) {
+		
+		Sleep(2000);
+		system("CLS");
+			a = to_string(rand()%9);
+			b = to_string(rand()%9);
+			if (a != b) {
+				c = a + b;
+				system(("\"Color " + c + "\"").c_str());
+				cout << "background: " << a << endl;
+				cout << "font color: " << b << endl;
+				cout << string(21, '=') << endl;
+				for (j = 0; j < 10; j++) cout << string(10 - j, ' ') << string(1 + (j * 2), '*') << endl;
+				i++;
+			}
 		}
 	}
-}
+
